f2c6c2a9323fbe10b0e3f85b02435b9ea81dedb4
Dursun KOC <dursunkoc@gmail.com>
2015-11-11 13:02:42 +0200

    configuration loaded

--- src/main/java/com/dursun/koc/helloakka/Main.java
+++ src/main/java/com/dursun/koc/helloakka/Main.java
@@ -3,6 +3,8 @@
  */
 package com.dursun.koc.helloakka;
 
+import com.typesafe.config.ConfigFactory;
+
 import akka.actor.ActorRef;
 import akka.actor.ActorSelection;
 import akka.actor.ActorSystem;
@@ -15,10 +17,11 @@ import akka.event.LoggingAdapter;
  *
  */
 public class Main {
-	private static final ActorSystem system = ActorSystem.create("Main");
+	private static final ActorSystem system = ActorSystem.create("Main", ConfigFactory.load().getConfig("Main"));
 	private static final LoggingAdapter LOG = Logging.getLogger(system, Main.class);
 	public static void main(String[] args) {
 		LOG.info("MAIN started!");
+		LOG.debug("ANY DEBUG LOG");
 		ActorRef a = system.actorOf(Props.create(HelloWorld.class), "helloWorld");
 		system.actorOf(Props.create(Terminator.class, a), "terminator");
 		ActorSelection actorSelection = system.actorSelection("user/helloWorld/greeter");
